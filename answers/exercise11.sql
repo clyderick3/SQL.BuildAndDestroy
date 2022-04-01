728cd40b0248ea1b1db539a85084b9d15437cc51
Clyde Broderick <clyderick3@gmail.com>
2022-04-01 18:21:04 -0400

    Completed 11.

---
+++ answers/exercise11.sql
@@ -0,0 +1,9 @@
+mysql> SELECT * FROM Students ORDER BY City ASC;
++-------------+--------------+--------------+------------+---------------+
+| StudentName | Address      | City         | PostalCode | Country       |
++-------------+--------------+--------------+------------+---------------+
+| Jane Doe    | 57 Union St  | Glasgow      | G13RB      | Scotland      |
+| June Dee    | 99 Union St  | Philadelphia | 19014      | United States |
+| Josh Dub    | 55 Crease Ln | Trenton      | 08618      | United States |
++-------------+--------------+--------------+------------+---------------+
+3 rows in set (0.01 sec)
