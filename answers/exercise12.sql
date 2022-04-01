8de2acce418b7320c15e173e7d17f3df841dbfd5
Clyde Broderick <clyderick3@gmail.com>
2022-04-01 18:23:56 -0400

    Completed exercise 12. Reversed by city.

---
+++ answers/exercise12.sql
@@ -0,0 +1,9 @@
+mysql> SELECT * FROM Students ORDER BY City DESC;
++-------------+--------------+--------------+------------+---------------+
+| StudentName | Address      | City         | PostalCode | Country       |
++-------------+--------------+--------------+------------+---------------+
+| Josh Dub    | 55 Crease Ln | Trenton      | 08618      | United States |
+| June Dee    | 99 Union St  | Philadelphia | 19014      | United States |
+| Jane Doe    | 57 Union St  | Glasgow      | G13RB      | Scotland      |
++-------------+--------------+--------------+------------+---------------+
+3 rows in set (0.01 sec)
