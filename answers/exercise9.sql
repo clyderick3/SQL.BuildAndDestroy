4de99194ac0e5b44565c5d3064f936656cc910d3
Clyde Broderick <clyderick3@gmail.com>
2022-04-01 01:56:10 -0400

    Completed exercise 9.

---
+++ answers/exercise9.sql
@@ -0,0 +1,6 @@
+mysql> SELECT * FROM Students WHERE City <> 'Philadelphia';
++-------------+-------------+---------+------------+----------+
+| StudentName | Address     | City    | PostalCode | Country  |
++-------------+-------------+---------+------------+----------+
+| Jane Doe    | 57 Union St | Glasgow | G13RB      | Scotland |
++-------------+-------------+---------+------------+----------+
