961914c5dc65b5b64c40a041fa54f8dfe52e830a
Clyde Broderick <clyderick3@gmail.com>
2022-04-01 00:03:08 -0400

    Truncated table Users.

---
+++ answers/exercise5.sql
@@ -0,0 +1,14 @@
+mysql> truncate table Users;
+Query OK, 0 rows affected (0.02 sec)
+
+mysql> describe Users;
++-----------+--------------+------+-----+---------+-------+
+| Field     | Type         | Null | Key | Default | Extra |
++-----------+--------------+------+-----+---------+-------+
+| UserID    | int          | YES  |     | NULL    |       |
+| LastName  | varchar(255) | YES  |     | NULL    |       |
+| FirstName | varchar(255) | YES  |     | NULL    |       |
+| Address   | varchar(255) | YES  |     | NULL    |       |
+| City      | varchar(255) | YES  |     | NULL    |       |
++-----------+--------------+------+-----+---------+-------+
+5 rows in set (0.01 sec)
