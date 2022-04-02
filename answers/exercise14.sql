26b3f2f480ed1209a47edba5caf79a0eaa1ac448
Clyde Broderick <clyderick3@gmail.com>
2022-04-01 21:48:49 -0400

    Completed 14.

---
+++ answers/exercise14.sql
@@ -0,0 +1,7 @@
+mysql> SELECT * FROM Students WHERE PostalCode IS NULL;
++-------------+-------------+-------+------------+---------------+
+| StudentName | Address     | City  | PostalCode | Country       |
++-------------+-------------+-------+------------+---------------+
+| Jack Doy    | 1818 Coy St | Dover | NULL       | United States |
++-------------+-------------+-------+------------+---------------+
+1 row in set (0.00 sec)
