5cd51459158edd6d3d08c27e77c34e96656a561c
Clyde Broderick <clyderick3@gmail.com>
2022-04-01 21:59:49 -0400

    Completed 15.

---
+++ answers/exercise15.sql
@@ -0,0 +1,9 @@
+mysql> SELECT * FROM Students WHERE PostalCode IS NOT NULL;
++-------------+--------------+--------------+------------+---------------+
+| StudentName | Address      | City         | PostalCode | Country       |
++-------------+--------------+--------------+------------+---------------+
+| Jane Doe    | 57 Union St  | Glasgow      | G13RB      | Scotland      |
+| June Dee    | 99 Union St  | Philadelphia | 19014      | United States |
+| Josh Dub    | 55 Crease Ln | Trenton      | 08618      | United States |
++-------------+--------------+--------------+------------+---------------+
+3 rows in set (0.00 sec)
